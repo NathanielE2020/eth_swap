695fb9272b73d0a373aa3f02b24bbb7da0c9f8b7
NathanielE2020 <103788015+NathanielE2020@users.noreply.github.com>
2022-10-16 00:29:33 +0100

    initial commit

---
+++ src/contracts/EthSwap.sol
@@ -0,0 +1,64 @@
+pragma solidity^0.5.6;
+
+import "./Token.sol";
+
+contract EthSwap {
+    string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint public rate = 100;
+
+    event TokensPurchased(
+        address accounts,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+        event TokensSold(
+        address accounts,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+
+   function buyTokens() public payable {
+       // Calculate the number of tokens to buy
+       uint tokenAmount = msg.value * rate;
+
+       //Require that ethSwap has enough tokens
+       require(token.balanceOf(address(this)) >= tokenAmount);
+      
+       //Transfer tokens to user
+       token.transfer(msg.sender, tokenAmount);
+      
+      // Emit an event
+       emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+   }
+
+   function sellTokens(uint _amount) public payable {
+      // Calculate the amount of Ether to redeem
+       uint etherAmount = _amount / rate;
+
+       // Require that EthSwap has enough Ether
+       require(address(this).balance >= etherAmount);
+
+       //Perform sale
+       token.transferFrom(msg.sender, address(this), _amount);
+       msg.sender.transfer(etherAmount);
+
+       // Emit an event
+       emit TokensSold(msg.sender, address(token), _amount, rate);
+   }
+
+
+
+
+
+
+
+}
